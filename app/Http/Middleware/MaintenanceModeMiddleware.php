d5b28289da887c99cc72da8b2ff8acf1db334b72
dhaydt <1992dayat@gmail.com>
2022-11-30 21:03:25 +0700

    rm maintenance mode

--- app/Http/Middleware/MaintenanceModeMiddleware.php
+++ app/Http/Middleware/MaintenanceModeMiddleware.php
@@ -11,15 +11,15 @@ class MaintenanceModeMiddleware
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $maintenance_mode = Helpers::get_business_settings('maintenance_mode') ?? 0;
-        if ($maintenance_mode) {
-            return redirect()->route('maintenance-mode');
-        }
+        // $maintenance_mode = Helpers::get_business_settings('maintenance_mode') ?? 0;
+        // if ($maintenance_mode) {
+        //     return redirect()->route('maintenance-mode');
+        // }
         return $next($request);
     }
 }
