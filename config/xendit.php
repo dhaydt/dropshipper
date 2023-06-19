3213f613f8f23973002544c3b68b249b3ec20a2e
dhaydt <1992dayat@gmail.com>
2023-06-19 15:38:15 +0700

    fix xendit apikey

--- config/xendit.php
+++ config/xendit.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'apikey' => env('XENDIT_API_KEY', 'xnd_development_sHlAPGQ9jjPplhy0KbuXvFAr4GCJ4lNtk4kvNBJqB0cy4SPHxEYkDYxU8TjKN'),
+    'apikey' => env('XENDIT_API_KEY', 'xnd_public_production_uJLo9PqPZrCm4ucM9t0ZuEsGSmPP47J6bSainzTIH3iUz5jBBgxBRMdxflz18wW'),
 ];
